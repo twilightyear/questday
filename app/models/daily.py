edbf329311699fa1dc60851d34c0e34231a6af75
Kay <80385994+twilightyear@users.noreply.github.com>
2026-08-23 14:31:40 +0000

    fix: set two partition for calendar

--- app/models/daily.py
+++ app/models/daily.py
@@ -3,6 +3,6 @@ from sqlalchemy import Integer, String, Boolean, ForeignKey, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database.orm import Base
 
-#Year 테이블 구조
+#Daily 테이블 구조
 class Todo(Base):
-    __tablename__ = 'year'
+    __tablename__ = 'daily'
--- app/models/day.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from sqlalchemy import Integer, String, Boolean, ForeignKey, DateTime, func
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from database.orm import Base
-
-#Day 테이블 구조
-class Todo(Base):
-    __tablename__ = 'day'
