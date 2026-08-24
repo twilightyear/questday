6160fd1e8122f1b3c6b1b8978f91de301a76faaf
Kay <80385994+twilightyear@users.noreply.github.com>
2026-08-24 13:52:40 +0000

    refactor: refactor get_calendars_handler() using dependency injection

--- app/routers/calendar.py
+++ app/routers/calendar.py
@@ -1,24 +1,21 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends
 from sqlalchemy import select
-from database.db_connection import SessionFactory
+from database.db_connection import SessionFactory, get_db
 from models.calendar import Calendar
 from auth.password import hash_password, verify_password
 from schema.calendar.calendar_request import CalendarCreateRequest, CalendarUpdateRequest
 from schema.calendar.calendar_response import CalendarResponse
+from sqlalchemy.orm import Session
 
 router = APIRouter(tags=["Calendar"])
 
-#모든 달력 가져오기
+#user_id 에 해당하는 달력 가져오기
 @router.get(
     "/calendars",
     response_model = list[CalendarResponse],
     status_code = status.HTTP_200_OK
 )
-def get_calendars_handler(user_id: int):
-    session = SessionFactory()
-    try:
-        stmt = select(Calendar).where(Calendar.user_id == user_id)
-        calendars = session.execute(stmt).scalars().all()
-        return calendars
-    finally:
-        session.close()
+def get_calendars_handler(user_id: int, session : Session = Depends(get_db)):
+    stmt = select(Calendar).where(Calendar.user_id == user_id)
+    calendars = session.execute(stmt).scalars().all()
+    return calendars
