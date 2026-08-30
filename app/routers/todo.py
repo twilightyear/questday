e2f2bce99a80d7004a065cdf500f15d58fd3d7c6
Kay <80385994+twilightyear@users.noreply.github.com>
2026-08-30 14:27:20 +0000

    feat(todo): add create_todo_handler()

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -84,6 +84,79 @@ def get_todos_handler(user_id: int, year: int, month: int, day: int, category_id
     return existing_todos
 
 #단일 Todo 생성
+@router.post(
+    "/users/{user_id}/calendars/{year}/dailies/{month}/{day}/category/{category_id}/todo",
+    response_model = TodoResponse,
+    status_code = status.HTTP_201_CREATED,
+    summary = "단일 Todo 생성"
+)
+def create_todo_handler(body: TodoCreateRequest, user_id: int, year: int, month: int, day: int, category_id: int, session : Session = Depends(get_db)):
+    #존재하는 사용자인지 검사 (404 NOT FOUND)
+    existing_user = session.execute(
+        select(User).where(User.user_id == user_id)
+    ).scalar_one_or_none()
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="존재하지 않는 사용자입니다."
+        )
+    
+    #존재하는 달력인지 검사 (404 NOT FOUND)
+    existing_calendar = session.execute(
+        select(Calendar).where(
+            Calendar.user_id == user_id,
+            Calendar.year == year
+        )
+    ).scalar_one_or_none()
+
+    if not existing_calendar:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="존재하지 않는 달력입니다."
+        )
+
+    #존재하는 날짜인지 검사 (404 NOT FOUND)
+    existing_daily = session.execute(
+        select(Daily).where(
+            Daily.calendar_id == existing_calendar.calendar_id,
+            Daily.month == month,
+            Daily.day == day
+        )
+    ).scalar_one_or_none()
+
+    if not existing_daily:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="존재하지 않는 날짜입니다."
+        )
+    
+    #존재하는 카테고리인지 검사 (404 NOT FOUND)
+    existing_category = session.execute(
+        select(Category).where(
+            Category.daily_id == existing_daily.daily_id,
+            Category.category_id == category_id
+        )
+    ).scalar_one_or_none()
+
+    if not existing_category:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="존재하지 않는 카테고리입니다."
+        )
+
+    todo = Todo(
+        category_id = existing_category.category_id,
+        title = body.title,
+        content = body.content,
+        is_done = body.is_done
+    )
+
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+
+    return todo
 
 #단일 Todo 삭제
 
