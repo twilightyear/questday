7b910e5f8ac3026a1113b1dfd4d6f021b3c085da
Kay <80385994+twilightyear@users.noreply.github.com>
2026-08-31 07:22:13 +0000

    refactor(router-api): apply dependency injection on user router api session

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends
 from sqlalchemy import select
-from database.db_connection import SessionFactory
+from database.db_connection import get_db
 from models.user import User
 from auth.password import hash_password, verify_password
 from schema.user.user_request import UserSignUpRequest, UserLoginRequest
 from schema.user.user_response import UserSignUpResponse
+from sqlalchemy.orm import Session
 
 router = APIRouter(tags=["User"]) #User 라우터
 
@@ -14,47 +15,46 @@ router = APIRouter(tags=["User"]) #User 라우터
     status_code = status.HTTP_201_CREATED,
     response_model = UserSignUpResponse
 )
-def signup_user_handler(body: UserSignUpRequest):
-    with SessionFactory() as session:
-        stmt = select(User).where(User.email == body.email)
-        existing_user = session.scalar(stmt)
+def signup_user_handler(body: UserSignUpRequest, session: Session = Depends(get_db)):
+    stmt = select(User).where(User.email == body.email)
+    existing_user = session.scalar(stmt)
 
-        if existing_user:
-            raise HTTPException(
-                status_code = status.HTTP_409_CONFLICT,
-                detail = "이미 가입된 이메일입니다."
-            )
-        hashed_password = hash_password(body.password)
-
-        user = User(
-            email = body.email,
-            hashed_password = hashed_password,
+    if existing_user:
+        raise HTTPException(
+            status_code = status.HTTP_409_CONFLICT,
+            detail = "이미 가입된 이메일입니다."
         )
 
-        session.add(user)
-        session.commit()
-        session.refresh(user)
-        
-        return user
+    hashed_password = hash_password(body.password)
+
+    user = User(
+        email = body.email,
+        hashed_password = hashed_password,
+    )
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
 
 #사용자 로그인
 @router.post(
     "/users/login",
     status_code = status.HTTP_200_OK
 )
-def login_user_handler(body: UserLoginRequest):
-    with SessionFactory() as session:
-        stmt = select(User).where(User.email == body.email)
-        user = session.scalar(stmt)
+def login_user_handler(body: UserLoginRequest, session: Session = Depends(get_db)):
+    stmt = select(User).where(User.email == body.email)
+    user = session.scalar(stmt)
 
-        if not user:
-            raise HTTPException(
-                status_code = status.HTTP_401_UNAUTHORIZED,
-                detail = "틀린 이메일 혹은 비밀번호입니다."
-            )
+    if not user:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "틀린 이메일 혹은 비밀번호입니다."
+        )
 
-        if not verify_password(body.password, user.hashed_password):
-            raise HTTPException(
-                status_code = status.HTTP_401_UNAUTHORIZED,
-                detail = "틀린 이메일 혹은 비밀번호입니다."
-            )
+    if not verify_password(body.password, user.hashed_password):
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "틀린 이메일 혹은 비밀번호입니다."
+        )
